17b87fdfb93537dbbd4a495f314674b6e69892ce
Takafumi Suzuki <highdro0479@gmail.com>
2019-11-13 12:36:11 +0900

    modify: clean _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,10 @@
 import NextApp from "next/app";
 import "../styles/global.scss";
 import Header from "../components/Header/Header";
+import { menuItemProps } from "../utils/menuItemProps";
 
 class MyApp extends NextApp {
   render() {
-    const menuItemProps = [
-      {
-        linkTo: "/",
-        label: "TOP",
-        labelEn: ""
-      },
-      {
-        linkTo: "/about",
-        label: "人",
-        labelEn: "about"
-      },
-      {
-        linkTo: "/works",
-        label: "作品",
-        labelEn: "works"
-      },
-      {
-        linkTo: "/skills",
-        label: "技術",
-        labelEn: "skill set"
-      }
-    ];
-
     const { Component } = this.props;
     return (
       <>
---
+++ utils/menuItemProps.ts
@@ -0,0 +1,22 @@
+export const menuItemProps = [
+  {
+    linkTo: "/",
+    label: "TOP",
+    labelEn: ""
+  },
+  {
+    linkTo: "/about",
+    label: "人",
+    labelEn: "about"
+  },
+  {
+    linkTo: "/works",
+    label: "作品",
+    labelEn: "works"
+  },
+  {
+    linkTo: "/skills",
+    label: "技術",
+    labelEn: "skill set"
+  }
+];
