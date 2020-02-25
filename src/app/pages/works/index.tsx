15d1c616fe006a20ca9a61425e0817ddfb9ba4b0
Takafumi Suzuki <highdro0479@gmail.com>
2020-02-26 00:44:47 +0900

    add: category select

--- src/app/pages/works/index.tsx
+++ src/app/pages/works/index.tsx
@@ -4,19 +4,29 @@ import { NextPage } from "next";
 import * as style from "../../styles/works.scss";
 import ImageHeader from "../../components/ImageHeader/ImageHeader";
 import axios from "axios";
-import { Post, Content } from "interfaces/Posts";
-import { useState } from "react";
+import { Post, Content, Category } from "interfaces/Posts";
+import { useState, useCallback } from "react";
 import Popup from "reactjs-popup";
 import WorkDetail from "../../components/workDetail/workDetail";
+import { Waypoint } from "react-waypoint";
 
 type Props = {
   contents: Content[];
+  categories: Category[];
   totalCount: number;
 };
 
 const WorksPage: NextPage<Props> = (props: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [currentId, setCurrentId] = useState("");
+  const [posts, setPosts] = useState<Content[]>(props.contents);
+  const [selectedCategory, setCategory] = useState<Object>(() => {
+    let entries = {};
+    props.categories.forEach(value => {
+      entries[value.id] = true;
+    });
+    return entries;
+  });
 
   const handleModalOpen = (id: string): void => {
     setCurrentId(id);
@@ -27,29 +37,44 @@ const WorksPage: NextPage<Props> = (props: Props) => {
     setIsOpen(false);
   };
 
-  const test = Array(100)
-    .fill({})
-    .map<Content>(() => ({
-      id: Math.random().toString(),
-      title: Math.random().toString()
-    }));
-  const [posts, setPosts] = useState<Content[]>(props.contents.concat(test));
+  const handleChipClicked = useCallback((id: string): void => {
+    setCategory(selectedCategory => {
+      const current = JSON.parse(JSON.stringify(selectedCategory));
+      current[id] = !selectedCategory[id];
+      return current;
+    });
+  }, []);
+
+  const switchChipStyle = (isActive: boolean): string => {
+    return isActive
+      ? `${style.categoryChip} ${style.active}`
+      : `${style.categoryChip}`;
+  };
+
+  const isActiveCategory = (categories: Category[]): boolean => {
+    let isActive = false;
+    categories.forEach(category => {
+      if (selectedCategory[category.id]) isActive = true;
+    });
+    return isActive;
+  };
 
   const onReload = async () => {
     const offset = posts.length;
     if (offset >= props.totalCount) {
       return;
     }
-    const key = {
-      headers: { "X-API-KEY": process.env.cms_api_key }
+    const parameters = {
+      headers: { "X-API-KEY": process.env.cms_api_key },
+      params: {
+        offset: offset,
+        fields: "id,title,cover_image,category"
+      }
     };
     await axios
-      .get<Post>(
-        `https://krgm4d.microcms.io/api/v1/works?offset=${offset}&fields=id,title,cover_image`,
-        key
-      )
+      .get<Post>(`https://krgm4d.microcms.io/api/v1/works`, parameters)
       .then(res => {
-        setPosts(posts.concat(res.data.contents));
+        setPosts(posts.concat(res.data.contents as Content[]));
       });
   };
 
@@ -59,21 +84,36 @@ const WorksPage: NextPage<Props> = (props: Props) => {
         imagePath="/static/images/works/cg_kirameki.png"
         text="作品"
       />
-      <button onClick={onReload}>reload</button>
-      <div className={style.contentsWrap}>
-        {posts.map(content => (
-          <a key={content.id} onClick={() => handleModalOpen(content.id)}>
-            <div className={style.workCard}>
-              <img
-                src={
-                  content.cover_image?.url ?? "/static/images/no_image256.png"
-                }
-                alt={content.title}
-              ></img>
-              <span className={style.workTitle}>{content.title}</span>
-            </div>
+      <div className={style.chipsWrap}>
+        {props.categories.map(item => (
+          <a
+            key={item.id}
+            onClick={() => handleChipClicked(item.id)}
+            className={switchChipStyle(selectedCategory[item.id])}
+          >
+            <p>{item.name}</p>
           </a>
         ))}
+      </div>
+      <div className={style.contentsWrap}>
+        {posts.map(
+          content =>
+            isActiveCategory(content.category) && (
+              <a key={content.id} onClick={() => handleModalOpen(content.id)}>
+                <div className={style.workCard}>
+                  <img
+                    src={
+                      content.cover_image?.url ??
+                      "/static/images/no_image256.png"
+                    }
+                    alt={content.title}
+                  ></img>
+                  <span className={style.workTitle}>{content.title}</span>
+                </div>
+              </a>
+            )
+        )}
+        <Waypoint onEnter={onReload}></Waypoint>
       </div>
       <Popup
         open={isOpen}
@@ -96,9 +136,28 @@ WorksPage.getInitialProps = async (): Promise<Props> => {
   const key = {
     headers: { "X-API-KEY": process.env.CMS_API_KEY }
   };
-  const res = await axios.get<Post>(
-    `https://krgm4d.microcms.io/api/v1/works?fields=id,title,cover_image`,
-    key
+
+  const contents = await axios.get<Post>(
+    `https://krgm4d.microcms.io/api/v1/works`,
+    {
+      params: {
+        fields: "id,title,cover_image,category",
+        limit: 1
+      },
+      ...key
+    }
   );
-  return { contents: res.data.contents, totalCount: res.data.totalCount };
+
+  const categories = await axios.get<Post>(
+    `https://krgm4d.microcms.io/api/v1/categories`,
+    {
+      ...key
+    }
+  );
+
+  return {
+    contents: contents.data.contents as Content[],
+    categories: categories.data.contents as Category[],
+    totalCount: contents.data.totalCount
+  };
 };
