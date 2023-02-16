169ebda9ba74f55d65f38fe4d0d3441d3a61ce19
Kurogoma4D <highdro0479@gmail.com>
2023-02-17 02:40:48 +0900

    CLEAN UP

--- src/app/components/spacer/spacer.tsx
+++
@@ -1,9 +0,0 @@
-import * as style from "./spacer.scss";
-
-type Props = {
-  height: string;
-};
-
-export const Spacer: React.FC<Props> = (props: Props) => {
-  return <div style={{ height: props.height }} className={style.spacer} />;
-};
--- src/app/utils/functions.ts
+++
@@ -1,9 +0,0 @@
-function normalizeNumber(min: number, max: number, data: number): number {
-  return (data - min) / (max - min);
-}
-
-function scaleNumber(min: number, max: number, data: number): number {
-  return data * (max - min) + min;
-}
-
-export { normalizeNumber, scaleNumber };
