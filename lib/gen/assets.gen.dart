0fb1c324d3865bc86f3e1acb24498ddafbe439d3
Kurogoma4D <highdro0479@gmail.com>
2023-02-17 21:50:09 +0900

    modify: add contents

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -8,6 +8,24 @@
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/github.svg
+  SvgGenImage get github => const SvgGenImage('assets/icons/github.svg');
+
+  /// File path: assets/icons/scrapbox.svg
+  SvgGenImage get scrapbox => const SvgGenImage('assets/icons/scrapbox.svg');
+
+  /// File path: assets/icons/twitter.svg
+  SvgGenImage get twitter => const SvgGenImage('assets/icons/twitter.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [github, scrapbox, twitter];
+}
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
@@ -22,6 +40,7 @@ class $AssetsImagesGen {
 class Assets {
   Assets._();
 
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
 }
 
@@ -84,6 +103,59 @@ class AssetGenImage {
   }
 
   ImageProvider provider() => AssetImage(_assetName);
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    ColorFilter? colorFilter,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated Clip? clipBehavior,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
 
   String get path => _assetName;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:portfolio/gen/assets.gen.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(
-        useMaterial3: true,
-      ),
+      title: 'Kurogoma4D',
+      theme: ThemeData.dark(useMaterial3: true),
       home: const _Contents(),
     );
   }
@@ -27,7 +27,72 @@ class _Contents extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Assets.images.avater.image(width: 120, height: 120),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Assets.images.avater.image(width: 120, height: 120),
+            const Gap(32),
+            Text(
+              'Kurogoma4D',
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const Gap(48),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _PreferredSizeIcon(
+                  url: 'https://scrapbox.io/kurogoma4d-lab/',
+                  child: Assets.icons.scrapbox.svg(height: 32),
+                ),
+                const Gap(24),
+                _PreferredSizeIcon(
+                  url: 'https://twitter.com/Krgm4D',
+                  child: Assets.icons.twitter.svg(
+                    height: 32,
+                    colorFilter: const ColorFilter.mode(
+                      Color(0xff1D9BF0),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+                const Gap(24),
+                _PreferredSizeIcon(
+                  url: 'https://github.com/Kurogoma4D',
+                  child: Assets.icons.github.svg(height: 32),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PreferredSizeIcon extends StatelessWidget {
+  const _PreferredSizeIcon({
+    required this.child,
+    required this.url,
+  });
+
+  final Widget child;
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => launchUrlString(url),
+      borderRadius: BorderRadius.circular(32),
+      child: SizedBox(
+        height: 32,
+        width: 32,
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: child,
+        ),
       ),
     );
   }
