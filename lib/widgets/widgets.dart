52d6bfff9eeff7a8e5180639b9c45dd2504a18c8
Kurogoma4D <highdro0479@gmail.com>
2026-02-06 13:40:34 +0900

    Migrate to Angular

--- lib/widgets/widgets.dart
+++
@@ -1,6 +0,0 @@
-export 'background_canvas.dart';
-export 'base.dart';
-export 'frosted_glass_effect.dart';
-export 'license_open_button.dart';
-export 'preferred_size_icon.dart';
-export 'profile.dart';
